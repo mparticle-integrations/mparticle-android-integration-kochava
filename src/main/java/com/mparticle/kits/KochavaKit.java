efc2c32fe6cfc21649513398419dfe599c6a5c33
Sam Dozor <sdozor@mparticle.com>
2017-01-18 16:24:35 -0500

    Update Kochava SDK to 20170106

--- src/main/java/com/mparticle/kits/KochavaKit.java
+++ src/main/java/com/mparticle/kits/KochavaKit.java
@@ -21,7 +21,6 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
     private static final String LIMIT_ADD_TRACKING = "limitAdTracking";
     private static final String RETRIEVE_ATT_DATA = "retrieveAttributionData";
     private static final String ENABLE_LOGGING = "enableLogging";
-    private static final String CURRENCY = "currency";
     private Feature feature;
 
     @Override
@@ -46,11 +45,7 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
 
     @Override
     public void setLocation(Location location) {
-        if (feature != null && location != null) {
-            feature.setLatlong(
-                    Double.toString(location.getLatitude()),
-                    Double.toString(location.getLongitude()));
-        }
+
     }
 
     @Override
@@ -117,11 +112,6 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
         if (feature == null) {
             HashMap<String, Object> datamap = new HashMap<String, Object>();
             datamap.put(Feature.INPUTITEMS.KOCHAVA_APP_ID, getSettings().get(APP_ID));
-            if (getSettings().containsKey(CURRENCY)) {
-                datamap.put(Feature.INPUTITEMS.CURRENCY, getSettings().get(CURRENCY));
-            } else {
-                datamap.put(Feature.INPUTITEMS.CURRENCY, "USD");
-            }
             datamap.put(Feature.INPUTITEMS.APP_LIMIT_TRACKING, getSettings().get(LIMIT_ADD_TRACKING));
             datamap.put(Feature.INPUTITEMS.DEBUG_ON, Boolean.parseBoolean(getSettings().get(ENABLE_LOGGING)));
             datamap.put(Feature.INPUTITEMS.REQUEST_ATTRIBUTION, Boolean.parseBoolean(getSettings().get(RETRIEVE_ATT_DATA)));

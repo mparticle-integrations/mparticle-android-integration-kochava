56ed9c596375de53f98a35bae18993d73796ee03
Will Passidomo <wpassidomo@mparticle.com>
2018-10-02 15:45:14 -0400

    Upgrade for to Kochava v3 - implement deep linking callbacks - switch to maven dependency for Kochava SDK

--- src/main/java/com/mparticle/kits/KochavaKit.java
+++ src/main/java/com/mparticle/kits/KochavaKit.java
@@ -4,9 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 
-import com.kochava.android.tracker.Feature;
-import com.kochava.android.tracker.ReferralCapture;
+import com.kochava.base.AttributionUpdateListener;
+import com.kochava.base.DeepLinkListener;
+import com.kochava.base.ReferralReceiver;
+import com.kochava.base.Tracker;
+import com.kochava.base.Tracker.Configuration;
+import com.mparticle.AttributionError;
+import com.mparticle.AttributionResult;
 import com.mparticle.MParticle;
+import com.mparticle.internal.Logger;
+import com.mparticle.internal.MPUtility;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -14,6 +24,8 @@ import java.util.List;
 import java.util.Map;
 
 public class KochavaKit extends KitIntegration implements KitIntegration.AttributeListener {
+    public static final String ATTRIBUTION_PARAMETERS = "attribution";
+    public static final String DEEPLINK_PARAMETERS = "deeplink";
 
     private static final String APP_ID = "appId";
     private static final String USE_CUSTOMER_ID = "useCustomerId";
@@ -21,14 +33,8 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
     private static final String LIMIT_ADD_TRACKING = "limitAdTracking";
     private static final String RETRIEVE_ATT_DATA = "retrieveAttributionData";
     private static final String ENABLE_LOGGING = "enableLogging";
-    private Feature feature;
 
     private static Map<String, String> identityLink;
-
-    @Override
-    public Object getInstance() {
-        return feature;
-    }
 
     @Override
     public String getName() {
@@ -37,10 +43,29 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
 
     @Override
     protected List<ReportingMessage> onKitCreate(Map<String, String> settings, Context context) {
-        createKochava();
-        if (feature != null) {
-            feature.setAppLimitTracking(Boolean.parseBoolean(getSettings().get(LIMIT_ADD_TRACKING)));
-            Feature.enableDebug(Boolean.parseBoolean(getSettings().get(ENABLE_LOGGING)));
+        boolean attributionEnabled = Boolean.parseBoolean(getSettings().get(RETRIEVE_ATT_DATA));
+
+        int logLevel = Tracker.LOG_LEVEL_NONE;
+        if (Boolean.parseBoolean(getSettings().get(ENABLE_LOGGING))) {
+            logLevel = Tracker.LOG_LEVEL_DEBUG;
+        }
+
+        Configuration configuration = new Configuration(context.getApplicationContext())
+                .setLogLevel(logLevel)
+                .setAppGuid(getSettings().get(APP_ID))
+                .setAppLimitAdTracking(Boolean.parseBoolean(getSettings().get(LIMIT_ADD_TRACKING)));
+
+        if (identityLink != null) {
+            configuration.setIdentityLink(new Tracker.IdentityLink().add(identityLink));
+        }
+        if (attributionEnabled) {
+            configuration.setAttributionUpdateListener(mAttributionListener);
+        }
+
+        Tracker.configure(configuration);
+
+        if (attributionEnabled) {
+            Tracker.setDeepLinkListener(getKitManager().getLaunchUri(), mDeepLinkListener);
         }
         return null;
     }
@@ -77,25 +102,23 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
 
     @Override
     public void setInstallReferrer(Intent intent) {
-        new ReferralCapture().onReceive(getContext(), intent);
+        new ReferralReceiver().onReceive(getContext(), intent);
     }
 
     @Override
     public void setUserIdentity(MParticle.IdentityType identityType, String id) {
-        if (feature != null) {
-            if (identityType == MParticle.IdentityType.CustomerId) {
-                if (!getSettings().containsKey(USE_CUSTOMER_ID) ||
-                        Boolean.parseBoolean(getSettings().get(USE_CUSTOMER_ID))) {
-                    Map<String, String> map = new HashMap<String, String>(1);
-                    map.put(identityType.name(), id);
-                    feature.linkIdentity(map);
-                }
-            } else {
-                if (Boolean.parseBoolean(getSettings().get(INCLUDE_ALL_IDS))) {
-                    Map<String, String> map = new HashMap<String, String>(1);
-                    map.put(identityType.name(), id);
-                    feature.linkIdentity(map);
-                }
+        if (identityType == MParticle.IdentityType.CustomerId) {
+            if (!getSettings().containsKey(USE_CUSTOMER_ID) ||
+                    Boolean.parseBoolean(getSettings().get(USE_CUSTOMER_ID))) {
+                Map<String, String> map = new HashMap<>(1);
+                map.put(identityType.name(), id);
+                Tracker.setIdentityLink(new Tracker.IdentityLink().add(map));
+            }
+        } else {
+            if (Boolean.parseBoolean(getSettings().get(INCLUDE_ALL_IDS))) {
+                Map<String, String> map = new HashMap<>(1);
+                map.put(identityType.name(), id);
+                Tracker.setIdentityLink(new Tracker.IdentityLink().add(map));
             }
         }
     }
@@ -110,37 +133,57 @@ public class KochavaKit extends KitIntegration implements KitIntegration.Attribu
         return null;
     }
 
-    private void createKochava() {
-        if (feature == null) {
-            HashMap<String, Object> datamap = new HashMap<String, Object>();
-            datamap.put(Feature.INPUTITEMS.KOCHAVA_APP_ID, getSettings().get(APP_ID));
-            datamap.put(Feature.INPUTITEMS.APP_LIMIT_TRACKING, getSettings().get(LIMIT_ADD_TRACKING));
-            datamap.put(Feature.INPUTITEMS.DEBUG_ON, Boolean.parseBoolean(getSettings().get(ENABLE_LOGGING)));
-            datamap.put(Feature.INPUTITEMS.REQUEST_ATTRIBUTION, Boolean.parseBoolean(getSettings().get(RETRIEVE_ATT_DATA)));
-            if (identityLink != null) {
-                datamap.put(Feature.INPUTITEMS.IDENTITY_LINK, identityLink);
-            }
-            feature = new Feature(getContext(), datamap);
-
-        }
-    }
-
     @Override
     public List<ReportingMessage> setOptOut(boolean optOutStatus) {
-        if (feature != null) {
-            feature.setAppLimitTracking(optOutStatus);
-            List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
-            messageList.add(
-                    new ReportingMessage(this, ReportingMessage.MessageType.OPT_OUT, System.currentTimeMillis(), null)
-                            .setOptOut(optOutStatus)
-            );
-            return messageList;
-        }else {
-            return null;
-        }
+        Tracker.setAppLimitAdTracking(optOutStatus);
+        List<ReportingMessage> messageList = new LinkedList<>();
+        messageList.add(
+                new ReportingMessage(this, ReportingMessage.MessageType.OPT_OUT, System.currentTimeMillis(), null)
+                        .setOptOut(optOutStatus)
+        );
+        return messageList;
     }
 
     public static void setIdentityLink(Map<String, String> identityLink) {
         KochavaKit.identityLink = identityLink;
     }
+
+    private void setAttributionResultParameter(String key, JSONObject value) {
+        try {
+            JSONObject parameters = new JSONObject().put(key, value);
+            AttributionResult result = new AttributionResult()
+                    .setServiceProviderId(getConfiguration().getKitId())
+                    .setParameters(parameters);
+            getKitManager().onResult(result);
+        } catch (JSONException e) {
+            AttributionError error = new AttributionError()
+                    .setServiceProviderId(getConfiguration().getKitId())
+                    .setMessage(e.getMessage());
+            getKitManager().onError(error);
+        }
+    }
+
+    private AttributionUpdateListener mAttributionListener = new AttributionUpdateListener() {
+        @Override
+        public void onAttributionUpdated(String s) {
+            try {
+                JSONObject attributionJson = new JSONObject(s);
+                setAttributionResultParameter(ATTRIBUTION_PARAMETERS, attributionJson);
+            } catch (JSONException e) {
+                AttributionError error = new AttributionError()
+                        .setMessage("unable to parse attribution JSON:\n " + s)
+                        .setServiceProviderId(getConfiguration().getKitId());
+                getKitManager().onError(error);
+            }
+        }
+    };
+
+    private DeepLinkListener mDeepLinkListener = new DeepLinkListener() {
+        @Override
+        public void onDeepLink(Map<String, String> map) {
+            if (!MPUtility.isEmpty(map)) {
+                setAttributionResultParameter(DEEPLINK_PARAMETERS, MPUtility.mapToJson(map));
+            }
+        }
+    };
 }
